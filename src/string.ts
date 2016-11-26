f562a3e3509bcd4e8ff1f2274c6f7e8605a4e38c
Anton Shakirzianov <Anton Shakirzianov>
2016-11-26 00:53:36 -0800

    Implemented basic builders

---
+++ src/string.ts
@@ -0,0 +1,19 @@
+import { Parser, Input, Success, Fail } from "./core";
+
+class StringInput implements Input<string> {
+    constructor(readonly stream: string) {}
+}
+
+export function prefix(str: string) {
+    return new PrefixParser(str);
+}
+
+class PrefixParser implements Parser<string, string> {
+    constructor(readonly prefix: string) {}
+
+    parse(input: Input<string>) {
+        return input.stream.lastIndexOf(this.prefix, 0) === 0 ?
+            new Success(this.prefix, new StringInput(input.stream.substr(this.prefix.length)), this.prefix.length)
+            : new Fail(this.prefix.length);
+    }
+}
