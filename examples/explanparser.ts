699203f2eaf2133608cd2c8e96a1b8068a1724bd
Anton Shakirzianov <Anton Shakirzianov>
2016-11-27 00:12:33 -0800

    String builder postfix operators

--- examples/explanparser.ts
+++ examples/explanparser.ts
@@ -11,17 +11,17 @@ import {
     recursive,
 } from "../src/fluentBuilder";
 
-const whiteSpace = string(" ").parser;
+const whiteSpaces = string(" ").atLeastOne();
 
-const digit = string("0").or("1").or("2").or("3").or("4").or("5").or("6").or("7").or("8").or("9").parser;
-const num = atLeastOne(digit).reduce((acc, s) => acc + s, "");
+const digit = string("0").or("1").or("2").or("3").or("4").or("5").or("6").or("7").or("8").or("9");
+const num = digit.atLeastOne();
 
 const add = string("+").parser;
 const sub = string("-").parser;
 const mult = string("*").parser;
 const div = string("/").parser;
 
-const trivia = startsWith(anyNumberOf(whiteSpace).reduce((acc, s) => acc + s, "").parser()).produce(Trivia);
+const trivia = startsWith(whiteSpaces.maybe().parser).produce(Trivia);
 
 const numLiteral = trivia.followedBy(num).produce(NumLiteral);
 const literal = numLiteral;
--- src/StringBuilder.ts
+++ src/StringBuilder.ts
@@ -1,8 +1,28 @@
 import { Parser, Input, Success, Fail } from "./Core";
-import { choice } from "./Operators";
+import { choice, zeroMore, oneMore, optional, not, and, adopt } from "./Operators";
 
 export function prefix(str: string): Parser<string, string> {
     return new PrefixParser(str);
+}
+
+export function string(str: string): StringParserBuilder {
+    return new StringParserBuilderImp(prefix(str));
+}
+
+export function notStarts(sob: StringOrParser): StringParserBuilder {
+    return new StringParserBuilderImp(adopt(not(parser(sob)), v => ""));
+}
+
+export function star(sob: StringOrParser): StringParserBuilder {
+    return new StringParserBuilderImp(adopt(zeroMore(parser(sob)), ss => ss.reduce((a, s) => a + s)));
+}
+
+export function plus(sob: StringOrParser): StringParserBuilder {
+    return new StringParserBuilderImp(adopt(oneMore(parser(sob)), ss => ss.reduce((a, s) => a + s)));
+}
+
+export function question(sob: StringOrParser): StringParserBuilder {
+    return new StringParserBuilderImp(adopt(optional(parser(sob)), op => op || ""));
 }
 
 export function stringInput(str: string): Input<string> {
@@ -27,17 +47,20 @@ export type StringOrParser = string | Parser<string, string>;
 function isParser(sob: StringOrParser): sob is Parser<string, string> {
     return typeof sob !== "string";
 }
+function parser(sob: StringOrParser): Parser<string, string> {
+    return isParser(sob) ? sob : prefix(sob);
+}
 
 export interface StringParserBuilder extends Parser<string, string> {
     readonly parser: Parser<string, string>;
     or(other: StringOrParser): StringParserBuilder;
+    atLeastOne(): StringParserBuilder;
+    anyNumber(): StringParserBuilder;
+    maybe(): StringParserBuilder;
+    not(): StringParserBuilder;
 }
 
-export function string(str: string): StringParserBuilder {
-    return new StringParserBuilderImp(prefix(str));
-}
-
-export class StringParserBuilderImp implements Parser<string, string> {
+class StringParserBuilderImp implements Parser<string, string> {
     constructor(readonly parser: Parser<string, string>) {}
 
     parse(input: Input<string>) {
@@ -45,6 +68,22 @@ export class StringParserBuilderImp implements Parser<string, string> {
     }
 
     or(other: StringOrParser) {
-        return new StringParserBuilderImp(choice(this.parser, isParser(other) ? other : prefix(other)));
+        return new StringParserBuilderImp(choice(this.parser, parser(other)));
+    }
+
+    atLeastOne() {
+        return plus(this.parser);
+    }
+
+    anyNumber() {
+        return star(this.parser);
+    }
+
+    maybe() {
+        return question(this.parser);
+    }
+
+    not() {
+        return notStarts(this.parser);
     }
 }
